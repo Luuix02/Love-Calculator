7761a7cd385cd188a4c67b99981e6fad94282183
Luisa Fernanda <lufvi02@gmail.com>
2024-05-04 23:01:54 -0500

    changes in var names

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function calculateLove () {
   const nameOne = document.getElementById("name1").value
   const nameTwo = document.getElementById("name2").value
 
-  const lovePorcentage = nameOne && nameTwo ?
+  const lovePercentage = nameOne && nameTwo ?
 ((nameOne + nameTwo).split('').reduce((acc, char) => acc + char.charCodeAt(0), 0) % 101) :
 null;
 
-const porcentageResult = document.getElementById("result");
-porcentageResult.style.color = lovePorcentage !== null ? "green" : "red";
-porcentageResult.textContent = lovePorcentage !== null ? `Tienes un ${lovePorcentage} de compatibilidad` : 'Por favor ingresa ambos nombres';
+const percentageResult = document.getElementById("result");
+percentageResult.style.color = lovePercentage !== null ? "green" : "red";
+percentageResult.textContent = lovePercentage !== null ? `Tienes un ${lovePercentage} de compatibilidad` : 'Por favor ingresa ambos nombres';
 
 
 
